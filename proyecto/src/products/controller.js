bd746fa479f01cafd30b0734715a778a9da9af6c
Reiyirak <pdsmej@gmail.com>
2023-05-05 21:38:10 -0600

    Setup response for all the requests of the products module

--- proyecto/src/products/controller.js
+++ proyecto/src/products/controller.js
@@ -1,34 +1,46 @@
 const debug = require('debug')('app:module-products-controller')
+const createError = require('http-errors')
 const { ProductsService } = require('./services')
+const { Response } = require('../common/reponse')
 
 module.exports.ProductsController = {
   getProducts: async (req, res) => {
     try {
-      let products = await ProductsService.getAll()
-      res.json(products)
+      let products = await ProductsService.getAll();
+      Response.success(res, 200, 'Lista de productos', products);
     } catch (error) {
-      debug(error)
-      res.status(500).json({ message: "Internal server error" })
+      debug(error);
+      Response.error(res);
     }
   },
+
   getProduct: async (req, res) => {
     try {
       const { params: { id } } = req;
       let product = await ProductsService.getById(id);
-      res.json(product)
+      if (!product) {
+        Response.error(res, new createError.NotFound())
+      } else {
+        Response.success(res, 200, `Producto ${id}`, product);
+      }
     } catch (error) {
       debug(error)
-      res.status(500).json({ message: "Internal server error" })
+      Response.error(res);
     }
   },
+
   createProduct: async (req, res) => {
     try {
       const { body } = req;
-      const insertedId = await ProductsService.create(body)
-      res.json(insertedId)
+      if (!body || Object.keys(body).length === 0) {
+        Response.error(res, new createError.BadRequest())
+      } else {
+        const insertedId = await ProductsService.create(body);
+        Response.success(res, 201, 'Producto creado', insertedId);
+      }
     } catch (error) {
       debug(error);
-      res.status(500).json({ message: "Internal server error" })
+      Response.error(res);
     }
   },
 }
